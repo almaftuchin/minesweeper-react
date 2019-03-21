607e47f61180c568e4f662a8151e34949dcaea59
Batman <lnmai@ucdavis.edu>
2019-03-20 23:27:51 -0700

    Added Styling & First Click Safe is Done

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -5,56 +5,55 @@ export class Board extends Component {
   constructor(props) {
     super(props);
     this.progress = 0;
+    this.height = props.height;
+    this.width = props.width;
+    this.mines = props.mines;
+    this.size = props.height * props.width;
     this.state = {
-      board: this.fillBoard(props.heigth, props.width, props.mines),
+      board: this.initBoard(),
       status: 'Good luck!',
-      heigth: props.heigth,
-      width: props.width,
-      mines: props.mines,
-      size: props.heigth * props.width
+      started: 0
     };
   }
 
-  fillBoard(h, w, m) {
-    let size = h * w;
-    let board = this.initBoard(size);
-    board = this.addMines(board, size, m);
-    let minesLocation = board[1];
-    board = board[0];
-    board = this.addNeighbor(board, size, w, minesLocation);
+  fillBoard(index) {
+    let board = this.initBoard();
+    board = this.addNeighbor(...this.addMines(board, index));
     return board;
   }
 
   newGame = () => {
     this.progress = 0;
     this.setState({
-      board: this.fillBoard(
-        this.state.heigth,
-        this.state.width,
-        this.state.mines
-      ),
-      status: 'Good luck!'
+      board: this.initBoard(),
+      status: 'Good luck!',
+      started: 0
     });
   };
 
   // Fill board with nada
-  initBoard = size => {
+  initBoard = () => {
     let board = [];
-    for (let i = 0; i < size; i++) {
-      board = [...board, { mine: 0, revealed: 0, flagged: 0, val: 0 }];
+    for (let i = 0; i < this.size; i++) {
+      board = [...board, { mine: 0, revealed: 0, flagged: 0, val: 0, boom: 0 }];
     }
     return board;
   };
 
   // Fill board with mines
-  addMines = (board, size, m) => {
+  addMines = (board, cellIndex) => {
     let i = 0,
       j = [];
+    const m = this.mines,
+      w = this.width;
     while (i < m) {
-      let index = this.randomNumber(size);
-      if (!board[index].mine) {
+      const index = this.randomNumber();
+      const square = [-(w + 1), -w, -(w - 1), -1, 0, 1, w - 1, w, w + 1].map(
+        val => index + val
+      );
+      // Because first click is always safe - make sure the square does not include first click's position
+      if (!board[index].mine && !square.includes(cellIndex)) {
         board[index].mine = 1;
-        board[index].val = -1;
         j = [...j, index];
         i++;
       }
@@ -63,8 +62,13 @@ export class Board extends Component {
   };
 
   // Fill board with moves
-  addNeighbor = (board, size, w, minesLocation) => {
+  // Original algorithm: traverse through board from first location to last location then add one for each bomb.
+  // New algorithm: map through each bomb and add one to neighbors
+  addNeighbor = (board, minesLocation) => {
+    const size = this.size;
+    const w = this.width;
     minesLocation.forEach(i => {
+      // top neighbors
       if (i >= w) {
         // top left
         if (
@@ -79,6 +83,7 @@ export class Board extends Component {
         if ((i - w + 1) % w !== 0 && !board[i - w + 1].mine)
           board[i - w + 1].val++;
       }
+      // bottom neighbors
       if (i < size - w) {
         // btm left
         if ((i + w - 1) % w !== w - 1 && !board[i + w - 1].mine)
@@ -99,10 +104,11 @@ export class Board extends Component {
     return board;
   };
 
-  randomNumber = size => Math.floor(Math.random() * size);
+  randomNumber = () => Math.floor(Math.random() * this.size);
 
-  boom = () => {
+  boom = index => {
     let copy = [...this.state.board];
+    copy[index].boom = 1;
     copy.forEach(cell => {
       cell.revealed = 1;
     });
@@ -112,10 +118,27 @@ export class Board extends Component {
     });
   };
 
+  handleClick = (index, props) => {
+    if (!this.state.started) {
+      this.setState(
+        {
+          started: 1,
+          board: this.fillBoard(index)
+        },
+        () => {
+          this.reveal(index, props);
+        }
+      );
+    } else {
+      this.reveal(index, props);
+    }
+  };
+
   reveal = (index, { mine, revealed, flagged, val }) => {
     if (revealed || flagged) return;
     if (mine) {
-      return this.boom();
+      // Kaboom T_T
+      return this.boom(index);
     }
     if (val === 0) {
       this.floodFill(index);
@@ -123,7 +146,8 @@ export class Board extends Component {
       let copy = [...this.state.board];
       this.progress++;
       copy[index].revealed = 1;
-      if (this.progress === this.state.size - this.state.mines) {
+      // Check if win
+      if (this.progress === this.size - this.mines) {
         copy.forEach(cell => {
           cell.revealed = 1;
         });
@@ -146,8 +170,8 @@ export class Board extends Component {
     this.setState({
       board: board
     });
-    let w = this.state.width;
-    let size = this.state.size;
+    const w = this.width,
+      size = this.size;
     if (i >= w) {
       // top left
       if (i - w - 1 >= 0 && (i - w - 1) % w !== w - 1)
@@ -171,12 +195,25 @@ export class Board extends Component {
     if (i + 1 < size && (i + 1) % w !== 0) this.reveal(i + 1, board[i + 1]);
   };
 
+  rightClick = (index, props, e) => {
+    e.preventDefault();
+    // If first click then reveal instead of flagging.
+    if (!this.state.started) return this.handleClick(index, props, e);
+    // Only flag-able if not revealed.
+    if (this.state.board[index].revealed) return;
+    let copy = [...this.state.board];
+    copy[index].flagged = !copy[index].flagged;
+    this.setState({
+      board: copy
+    });
+  };
+
   renderBoard = () => {
     return this.state.board.map((props, index) => {
       return (
         <Cell
           key={index}
-          onClick={() => this.reveal(index, props)}
+          onClick={() => this.handleClick(index, props)}
           onContextMenu={e => this.rightClick(index, props, e)}
           cell={props}
         />
